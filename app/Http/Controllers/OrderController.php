cb6e3dcb728a962af5e60aa4f5ca8c53d1bc5385
mvhung <maihung1372002@gmail.com>
2023-12-19 21:32:44 +0700

    config JWT auth

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api');
+    // }
     public function index()
     {
         $orders = Orders::all();
